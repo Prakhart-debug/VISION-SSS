463addae0119d6fed1c91cb63818c87faf6005a5
Prakhar Tripathi <75558721+Prakhart-debug@users.noreply.github.com>
2021-10-31 12:51:04 +0530

    Add files via upload

---
+++ boom.ino
@@ -0,0 +1,25 @@
+#include <SPI.h>
+#include <SD.h>
+Serial.begin(9600);
+
+Serial.println(value);
+import processing.serial.*;
+Serial mySerial;
+PrintWriter output;
+void setup() {
+mySerial = new Serial( this, Serial.list()[0], 9600 );
+output = createWriter( "data.txt" );
+}
+void draw() {
+if (mySerial.available() > 0 ) {
+String value = mySerial.readString();
+if ( value != null ) {
+output.println( value );
+}
+}
+}
+void keyPressed() {
+output.flush(); // Writes the remaining data to the file
+output.close(); // Finishes the file
+exit(); // Stops the program
+}
